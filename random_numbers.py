31fd62833b470ae1837b1546fe613c06269b30a7
Astenik <106985461+Astenik@users.noreply.github.com>
2022-08-13 13:16:26 +0400

    Update random_numbers.py

--- random_numbers.py
+++ random_numbers.py
@@ -1,4 +1,4 @@
-'''this project generates 40000000 numbers in a file, and that file reservet not larger than 4GB memory space.'''
+'''this project generates 4* 10**9 numbers in a file, and that file reservet not larger than 4GB memory space.'''
 import random
 new_file = open('rand_num', 'w')
 lst = []
@@ -7,7 +7,7 @@ for num in range(1, 100):
      lst.append(num)
 for num1 in range(100, 201):
      new_lst.append(num1)
-for num1 in range(1, 20000000):
+for num1 in range(1, 2 * 10**9):
      num = new_file.write(str(random.choice(new_lst)))
      num = new_file.write(' ')
      num = new_file.write(str(random.choice(lst)))
